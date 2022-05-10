6af5b59b218c8fc160233b3e668b23537cf74c7a
Masaya Kazama <miyan@t-p.jp>
2022-05-10 22:04:15 +0900

    work countをテキストに同期

--- youtube_chapter.lua
+++ youtube_chapter.lua
@@ -5,6 +5,7 @@ start_time = nil
 work_scene = nil
 work_count = 0
 output_path = ""
+work_count_source = ""
 
 function get_chapter_text(time, scene_name, count)
 	local seconds       = math.floor(time % 60)
@@ -17,6 +18,17 @@ function get_chapter_text(time, scene_name, count)
 	end
 end
 
+function update_count_text()
+	local source = obs.obs_get_source_by_name(work_count_source)
+	if source ~= nil then
+		local settings = obs.obs_data_create()
+		obs.obs_data_set_string(settings, "text", string.format("#%d", work_count))
+		obs.obs_source_update(source, settings)
+		obs.obs_data_release(settings)
+		obs.obs_source_release(source)
+	end
+end
+
 function print_chapter(time)
 	local scene = obs.obs_frontend_get_current_scene()
 	local scene_name = obs.obs_source_get_name(scene)
@@ -24,6 +36,8 @@ function print_chapter(time)
 	if scene_name == work_scene then
 		work_count = work_count + 1
 	end
+
+	update_count_text()
 
 	local line = get_chapter_text(time, scene_name, work_count)
 	io.write(line, "\n")
@@ -49,6 +63,9 @@ function on_event(event)
 		if output_path ~= "" then
 			io.close()
 		end
+
+		work_count = 0
+		update_count_text()
 	end
 
 	-- 配信中のシーン変更時、開始時刻との差分秒数とシーン名を書き出す
@@ -79,6 +96,20 @@ function script_properties()
 	-- 保存先選択
 	obs.obs_properties_add_path(props, "output_path", "Output path", obs.OBS_PATH_FILE,  "text file (*.txt)", nil)
 
+	-- workカウント表示
+	local p = obs.obs_properties_add_list(props, "work_count_source", "Work count source", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
+	local sources = obs.obs_enum_sources()
+	if sources ~= nil then
+		for _, source in ipairs(sources) do
+			source_id = obs.obs_source_get_unversioned_id(source)
+			if source_id == "text_gdiplus" or source_id == "text_ft2_source" then
+				local name = obs.obs_source_get_name(source)
+				obs.obs_property_list_add_string(p, name, name)
+			end
+		end
+	end
+	obs.source_list_release(sources)
+
 	return props
 end
 
@@ -86,6 +117,8 @@ end
 function script_update(settings)
 	work_scene = obs.obs_data_get_string(settings, "work_scene")
 	output_path = obs.obs_data_get_string(settings, "output_path")
+	work_count_source = obs.obs_data_get_string(settings, "work_count_source")
+	update_count_text()
 end
 
 
